50bee1cbb0f0475d2f80fc11eb27c1be1034989b
cesparza2022 <100377746+cesparza2022@users.noreply.github.com>
2022-12-04 23:29:57 -0600

    Ordenar el repositorio

--- data/Dowload_Files.py
+++ data/Dowload_Files.py
@@ -1,3 +1,3 @@
 import wget
 url = 'http://aging.brain-map.org/api/v2/well_known_file_download/502999992'
-filename = wget.download(url)
+filename = wget.download(url, out = "BioPython_project/data/")
