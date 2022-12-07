60289e29c83a588e49e1484445aedbe36723151f
cesparza2022 <100377746+cesparza2022@users.noreply.github.com>
2022-12-06 20:08:52 -0600

    Borrador gráfica 2

---
+++ src/grafica2.py
@@ -0,0 +1,20 @@
+import pandas as pd
+import argparse
+
+arg_parser = argparse.ArgumentParser("Número de genes por cromosoma secuenciados")
+
+arg_parser.add_argument("-f", "--FILE",
+                    metavar="path/to/file",
+                    help="archivo con expresión génica",
+                    required=True)
+
+args = arg_parser.parse_args()
+
+dataset_df = pd.read_csv(args.FILE)
+
+str_col = dataset_df["structure_name"].values.tolist()
+
+tmp_crt = str_col.count("temporal neocortex")
+w_mtr = str_col.count("white matter of forebrain")
+hipp = str_col.count("hippocampus (hippocampal formation)")
+par_neo = str_col.count("parietal neocortex")
