181c46a5cb932290089e50b5d8a7bf279a369960
cesparza2022 <100377746+cesparza2022@users.noreply.github.com>
2022-12-08 11:11:35 -0600

    Corregir identación

--- src/grafica_gen_por_cromosoma.py
+++ src/grafica_gen_por_cromosoma.py
@@ -19,8 +19,10 @@ crom_gen_df = pd.read_csv(args.FILE)
 ##Hacer el loop para recuperar que recorra el numero de rows por columna 
 n = 1
 gen_crom = []
-cromosomas = ["1","2","3","4","5","6","7","8","9"        ,"10","11","12","13","14","15","16","17","18","19"
-,"20","21","22","X","Y","MT"]
+cromosomas = ["1","2","3","4","5","6","7","8","9" 
+              ,"10","11","12","13","14","15","16"
+              ,"17","18","19","20","21","22","X",
+              "Y","MT"]
 
 for gen in crom_gen_df.index:
   if (crom_gen_df["chromosome"][gen] != cromosomas[n]):
